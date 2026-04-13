60c97e261b0e3a2d11062ab043d9db08a66bfcc7
Kaifkhan-Code <soulflaming@gmail.com>
2026-04-13 13:13:59 +0530

    fixed some parts

--- cloudConfig.js
+++ cloudConfig.js
@@ -3,20 +3,27 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const cloudinary = require("cloudinary").v2;
+
+// ✅ IMPORTANT FIX
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
+// config
 cloudinary.config({
-  cloud_name: process.env.Cloud_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
+// storage
 const storage = new CloudinaryStorage({
-  cloudinary,
+  cloudinary: cloudinary,
   params: {
     folder: "wanderlust_DEV",
     allowed_formats: ["jpeg", "png", "jpg"],
   },
 });
 
-module.exports = { cloudinary, storage };
+module.exports = {
+  cloudinary,
+  storage,
+};
